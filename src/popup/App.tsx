e8869f2341e13d1ac6f72e72651e0e9894422aac
neo <shaokexu@example.com>
2023-01-09 00:54:51 -0800

    implement menu list

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,19 +1,19 @@
-import { HomeOutlined, StarOutlined } from "@ant-design/icons";
+import {
+  DeleteOutlined,
+  LoadingOutlined,
+  SearchOutlined,
+  SettingOutlined,
+  StarOutlined
+} from "@ant-design/icons";
 import { useMount } from "ahooks";
-import { Button, Card, Input } from "antd";
-import React, { useEffect, useState } from "react";
+import { Avatar, Button, Card, List } from "antd";
+import React, { useState } from "react";
+import browser from "webextension-polyfill";
 
 import { checkWhetherBookmarked } from "~packages/apis/bookmarks";
-import Logo from "~packages/components/logo";
 import { getActiveTab } from "~packages/helpers/tab";
 
 import "./app.less";
-
-const { Search } = Input;
-
-const onSearch = (value: string) => console.log(value);
-
-const goHomePage = () => {};
 
 enum BOOKMARK_STATUS {
   Loading,
@@ -21,8 +21,84 @@ enum BOOKMARK_STATUS {
   NotBookmarked
 }
 
+enum MENU_KEYS {
+  DeleteBookmark,
+  AddBookmark,
+  Search
+}
+
+function getItem(optionObj: {
+  title: React.ReactNode;
+  key: React.Key;
+  icon: React.ReactNode;
+  description: React.ReactNode;
+  click: Function;
+}) {
+  return optionObj;
+}
+
 const App: React.FC = function App() {
   const [isBookmarked, setIsBookmarked] = useState(BOOKMARK_STATUS.Loading);
+
+  const closePopup = () => window.close();
+
+  const goSettingsPage = () => {
+    browser.tabs.create({
+      url: `/options.html#/settings`
+    });
+    closePopup();
+  };
+
+  const goSearchPage = () => {
+    browser.tabs.create({
+      url: `/options.html#/search`
+    });
+    closePopup();
+  };
+
+  const deleteBookmark = async () => {
+    console.log(`deleteBookmark`);
+  };
+
+  const addBookmark = async () => {
+    console.log(`addBookmark`);
+  };
+
+  let items = [];
+  if (isBookmarked === BOOKMARK_STATUS.Bookmarked) {
+    items.push(
+      getItem({
+        title: chrome.i18n.getMessage("deleteBookmark"),
+        description: "option+s",
+        key: MENU_KEYS.DeleteBookmark,
+        icon: <DeleteOutlined />,
+        click: deleteBookmark
+      })
+    );
+  } else {
+    let option = {
+      title: chrome.i18n.getMessage("addBookmark"),
+      description: "option+s",
+      key: MENU_KEYS.AddBookmark,
+      icon: <StarOutlined />,
+      click: addBookmark
+    };
+    if (isBookmarked === BOOKMARK_STATUS.Loading) {
+      option.click = async () => {};
+      option.icon = <LoadingOutlined />;
+    }
+    items.push(getItem(option));
+  }
+
+  items.push(
+    getItem({
+      title: chrome.i18n.getMessage("search"),
+      description: "option+f",
+      key: MENU_KEYS.Search,
+      icon: <SearchOutlined />,
+      click: goSearchPage
+    })
+  );
 
   useMount(() => {
     const fetchData = async () => {
@@ -34,9 +110,7 @@ const App: React.FC = function App() {
         setIsBookmarked(BOOKMARK_STATUS.NotBookmarked);
       }
     };
-    // setIsBookmarked(BOOKMARK_STATUS.Loading);
     fetchData().catch((err) => {
-      console.error(err);
       setIsBookmarked(BOOKMARK_STATUS.NotBookmarked);
     });
   });
@@ -44,36 +118,37 @@ const App: React.FC = function App() {
   return (
     <div className="popup">
       <Card
-        title={
-          <>
-            <Logo className="card-head-title-action" />
-            <Search
-              className="card-head-title-action"
-              placeholder="Please type your search context"
-              allowClear
-              onSearch={onSearch}
-              style={{ width: 400 }}
-            />
-          </>
-        }
+        title={chrome.i18n.getMessage("extensionName")}
         extra={
           <>
             <Button
-              type={
-                isBookmarked === BOOKMARK_STATUS.Bookmarked
-                  ? "primary"
-                  : "default"
-              }
-              className="card-extra-action"
-              icon={<StarOutlined />}
-              loading={isBookmarked === BOOKMARK_STATUS.Loading ? true : false}
+              type="link"
+              block
+              icon={<SettingOutlined />}
+              onClick={goSettingsPage}
+              title={chrome.i18n.getMessage("tooltipSettingIcon")}
             />
-            <Button className="card-extra-action" icon={<HomeOutlined />} />
           </>
         }
         className="card-without-radius">
-        <p>Card content</p>
-        <p>Card content</p>
+        <List
+          itemLayout="horizontal"
+          dataSource={items}
+          renderItem={(item) => (
+            <List.Item onClick={() => item.click(item)}>
+              <List.Item.Meta
+                avatar={
+                  <Avatar
+                    style={{ backgroundColor: "#1890ff" }}
+                    icon={item.icon}
+                  />
+                }
+                title={item.title}
+                description={item.description}
+              />
+            </List.Item>
+          )}
+        />
       </Card>
     </div>
   );
