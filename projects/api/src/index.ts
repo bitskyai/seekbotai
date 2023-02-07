3896bd68477c682586a4485b3986871641cc50a2
neo <shaokexu@example.com>
2023-02-06 19:23:59 -0800

    add express

--- projects/api/src/index.ts
+++ projects/api/src/index.ts
@@ -1,11 +1,17 @@
-import { createYoga } from 'graphql-yoga'
-import { createServer } from 'node:http'
-import {schema} from "./schema";
-// 2
-const port = Number(process.env.API_PORT) || 4000
-const yoga = createYoga({ schema })
-const server = createServer(yoga);
-// 4
-server.listen(port, () => {
-  console.info('Server is running on http://localhost:4000/graphql')
-})
+import express from "express";
+import { createYoga } from "graphql-yoga";
+import path from "path";
+
+import { schema } from "./schema";
+
+const port = Number(process.env.API_PORT) || 4000;
+const app = express();
+
+const yoga = createYoga({ schema });
+app.use(express.static(path.join(__dirname + "/public")));
+app.use(express.static(path.join(__dirname + "/ui")));
+app.use("/graphql", yoga);
+
+app.listen(port, () => {
+  console.info(`Server is running on http://localhost:${port}`);
+});
