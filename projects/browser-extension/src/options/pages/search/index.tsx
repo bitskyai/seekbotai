9ed0acdcc72cb13341bf36e67cf84c76682e1fd9
neo <shaokexu@example.com>
2023-10-12 08:42:55 -0700

    fix options page doesn't render issue

--- projects/browser-extension/src/options/pages/search/index.tsx
+++ projects/browser-extension/src/options/pages/search/index.tsx
@@ -1,21 +1,22 @@
 // import { useQuery } from "@apollo/client";
 
 import {
-  APP_DISPLAY_EXTENSION_SETTINGS_OPTION,
-  APP_NAVIGATE_TO_EXTENSION_SETTINGS,
-  APP_READY_MESSAGE,
+  // APP_DISPLAY_EXTENSION_SETTINGS_OPTION,
+  // APP_NAVIGATE_TO_EXTENSION_SETTINGS,
+  // APP_READY_MESSAGE,
   DEFAULT_HOST_NAME,
   DEFAULT_PROTOCOL
 } from "@bitsky/shared"
-import { useEffect, useRef } from "react"
-import { useNavigate } from "react-router-dom"
+import { useRef } from "react"
+
+// import { useNavigate } from "react-router-dom"
 
 import { useStorage } from "@plasmohq/storage/hook"
 
 import { StorageKeys } from "~storage"
 
 export default function Search() {
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
   const iframeElem = useRef(null)
 
   const [protocol] = useStorage(StorageKeys.ServiceProtocol)
@@ -26,23 +27,25 @@ export default function Search() {
     hostName ?? DEFAULT_HOST_NAME
   }:${port}`
 
-  useEffect(() => {
-    console.log("search init message listener")
-    window.removeEventListener("message", () => console.log("remove message"))
-    window.addEventListener("message", function (event) {
-      if (event.data === APP_READY_MESSAGE) {
-        iframeElem?.current?.contentWindow.postMessage(
-          APP_DISPLAY_EXTENSION_SETTINGS_OPTION,
-          "*"
-        )
-      } else if (event.data === APP_NAVIGATE_TO_EXTENSION_SETTINGS) {
-        navigate("/settings")
-      }
-    })
-  }, [])
+  // TODO: review whether we need this feature
+  // useEffect(() => {
+  //   console.log("search init message listener")
+  //   window.removeEventListener("message", () => console.log("remove message"))
+  //   window.addEventListener("message", function (event) {
+  //     console.log("search message listener, event: ", event)
+  //     if (event.data === APP_READY_MESSAGE) {
+  //       iframeElem?.current?.contentWindow.postMessage(
+  //         APP_DISPLAY_EXTENSION_SETTINGS_OPTION,
+  //         "*"
+  //       )
+  //     } else if (event.data === APP_NAVIGATE_TO_EXTENSION_SETTINGS) {
+  //       navigate("/settings")
+  //     }
+  //   })
+  // }, [])
 
   return (
-    <div style={{}}>
+    <div>
       <iframe
         id="test"
         ref={iframeElem}
