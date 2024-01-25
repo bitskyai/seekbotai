dc76b15853f10c3ba6a7ee332a563c29375d00fc
neo <shaokexu@example.com>
2024-01-25 09:11:42 -0800

    update settings

--- projects/desktop-app/src/ui-src/pages/settings/index.tsx
+++ projects/desktop-app/src/ui-src/pages/settings/index.tsx
@@ -35,6 +35,7 @@ export default function Settings() {
       <Title level={4}>Settings</Title>
       <div>
         <ProForm
+          grid={true}
           initialValues={appPreferences}
           onValuesChange={(_, values) => {
             console.log(values);
@@ -49,56 +50,59 @@ export default function Settings() {
         >
           <ProFormGroup title="Desktop App">
             <ProFormText
-              width="lg"
               disabled
               name="DESKTOP_APP_USER_DATA_PATH"
-              label="User Data Path"
+              label="User Data Folder"
+              tooltip="This is the path where the SeekBot Desktop App stores its data, including the database, logs, screenshots, etc. If you change it, it will automatically move all your data to the new location and delete old folder."
               rules={[{ required: true, message: "You must select a folder" }]}
             />
           </ProFormGroup>
           <ProFormGroup title="API Service">
             <ProFormText.Password
-              width={"lg"}
               name="WEB_APP_MASTER_KEY"
               label="API Key"
+              tooltip="API key is used to authenticate requests to the SeekBot, if you change it, make sure you also update in the SeekBot browser extension."
               rules={[{ required: true, message: "You must set an API key" }]}
             />
-
             <ProFormSelect
               name="WEB_APP_LOG_LEVEL"
               label="Log Level"
+              width="md"
               valueEnum={{
                 error: "Error",
                 warn: "Warn",
                 info: "Info",
                 debug: "Debug",
               }}
+              tooltip="Log level is the minimum level of log messages that are written to the log file. For example, if you set it to 'Info', then only 'Error', 'Warn', and 'Info' messages will be written to the log file."
               placeholder="Please select a log level"
               rules={[
                 { required: true, message: "You must select a log level" },
               ]}
             />
-
             <ProFormDigit
               name="WEB_APP_LOG_MAX_SIZE"
               label="Log Max Size"
+              width="md"
               placeholder="Please type in a log max size"
+              tooltip="Log max size is the maximum size of the log file in megabytes. When the log file reaches this size, it will be rotated."
               rules={[
                 { required: true, message: "You must type a log max size" },
               ]}
             />
           </ProFormGroup>
           <ProFormGroup title="Search Engine">
-            <ProFormText.Password
+            {/* <ProFormText.Password
               width={"lg"}
               name="SEARCH_ENGINE_MASTER_KEY"
               label="API Key"
               rules={[{ required: true, message: "You must set an API key" }]}
-            />
+            /> */}
             <ProFormDigit
               name="SEARCH_ENGINE_INDEXING_FREQUENCY"
-              width={"lg"}
+              width={"md"}
               label="Indexing Frequency"
+              tooltip="Indexing frequency is the number of seconds between each indexing run. More frequent indexing will make the search results more accurate, but it will also consume more resources."
               placeholder="Please type in an indexing frequency"
               rules={[
                 {
